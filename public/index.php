e050fe2eb98f2727d543d9616f525e7e852d7032
Stephane_tcha <stephanetcha@gmail.com>
2023-01-07 14:31:15 +0100

    autoload, mise en place du seule require_once dans le index.js

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Email;
+
+$email = new Email();
+var_dump($email); 
--- src/Email.php
+++ src/Email.php
@@ -4,5 +4,16 @@ namespace App;
 
 class Email
 {
+    private string $email; 
 
+    public function getEmail(): string 
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self 
+    {
+        $this->email = $email;
+        return $this; 
+    }
 }
